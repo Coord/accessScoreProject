164c9a8d9f85fc05d99ae988fb15f1c444fdded2
Coord <coordinater@gmail.com>
2017-09-23 20:42:39 -0700

    Initial page

---
+++ assets/js/charts.js
@@ -0,0 +1,41 @@
+var config = {
+    title: {
+        text: '15%',
+        style: {
+            fontSize: '40px'
+        },
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 15
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        size: "100%",
+  			innerSize: "90%",
+  			pointPadding: 0,
+  			groupPadding: 0,
+        data: [
+            ['Firefox',   10.38],
+            ['IE',       56.33]
+        ]
+    }],
+    credits: {
+      enabled: false
+    }
+}
+
+var selectors = ['age', 'gender', 'race', 'income'];
+selectors.forEach(s => {
+    Highcharts.chart(s, config);
+});
